3f52a9b1da12acebcc1fc1f3a1a58302d1d22fd7
IohanaM <iohana.ma31@gmail.com>
2024-02-21 09:35:59 -0300

    populando as tabelas

---
+++ seeders/20240221011852-demo-coordenadores.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('coordenadoresArea',[
+      {
+        matricula: '789012',
+        nome: 'Pedro Santos',
+        email: 'pedro.santos@example.com',
+        area: 'Estética'
+      },
+      {
+        matricula: '123456',
+        nome: 'Ana Oliveira',
+        email: 'ana.oliveira@example.com',
+        area: 'Gastronomia'
+      },
+      {
+        matricula: '345678',
+        nome: 'Mariana Silva',
+        email: 'mariana.silva@example.com',
+        area: 'Tecnologia'
+      },
+      {
+        matricula: '901234',
+        nome: 'Carlos Sousa',
+        email: 'carlos.sousa@example.com',
+        area: 'Turismo'
+      }
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Remover os dados inseridos
+    await queryInterface.bulkDelete('coordenadoresArea', null, {});
+  }
+};
