9583c2db00bbb130e5c0f605e5e6a1a7ac739104
Iohana <iohana.ma31@gmail.com>
2024-03-07 16:43:36 -0300

    trocando a porta do back

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware
 app.use(cors());
