a060cfae3842934aab47b23961d5b01cf7882305
iohana <iohana.ma31@gmail.com>
2024-05-16 13:59:17 -0300

    fix: index com import.

--- GestaoInstrutoriaBack/index.js
+++ GestaoInstrutoriaBack/index.js
@@ -1,45 +1,27 @@
-import cors from 'cors';
-import express, { json, urlencoded } from 'express';
-import { serve, setup } from 'swagger-ui-express';
-import { authenticate, sync } from './config/connection.js';
-import servicoRouter from './modules/administrador/routes/servicoRoutes.js';
-import coordAreaRouter from './modules/coordenador/routes/coordAreaRoutes.js';
-import instrutorRouter from './modules/instrutor/routes/instrutorRoutes.js';
-import swaggerDoc from './swagger.json';
-
-// Importando métricas
-import { register, httpRequestDurationMicroseconds } from './metrics';
+const express = require('express');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const sequelize = require('./config/connection.js');
+const servicoRouter = require('./modules/administrador/routes/servicoRoutes.js');
+const coordAreaRouter = require('./modules/coordenador/routes/coordAreaRoutes.js');
+const instrutorRouter = require('./modules/instrutor/routes/instrutorRoutes.js');
+const swaggerDoc = require('./swagger.json');
 
 const app = express();
 const port = 3001;
 
 // Middleware
 app.use(cors());
-app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Endpoint de métricas para Prometheus
-app.get('/metrics', async (req, res) => {
-  res.set('Content-Type', register.contentType);
-  res.end(await register.metrics());
-});
-
-// Middleware para medir a duração de requisições HTTP
-app.use((req, res, next) => {
-  const end = httpRequestDurationMicroseconds.startTimer();
-  res.on('finish', () => {
-    end({ route: req.path, code: res.statusCode, method: req.method });
-  });
-  next();
-});
-
-// Rotas
+// Rotas dos módulos
 app.use('/servico', servicoRouter);
 app.use('/instrutor', instrutorRouter);
 app.use('/coordArea', coordAreaRouter);
 
 // Swagger UI
-app.use('/api-docs', serve, setup(swaggerDoc));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // Middleware para tratamento de erros
 app.use((err, req, res, next) => {
@@ -50,9 +32,9 @@ app.use((err, req, res, next) => {
 // Função assíncrona para autenticar o Sequelize e sincronizar as tabelas
 async function iniciarServidor() {
   try {
-    await authenticate();
+    await sequelize.authenticate();
     console.log('Conexão com o banco de dados estabelecida com sucesso.');
-    await sync();
+    await sequelize.sync();
     console.log('Tabelas sincronizadas com sucesso.');
     // Inicia o servidor após a sincronização das tabelas
     app.listen(port, () => {
