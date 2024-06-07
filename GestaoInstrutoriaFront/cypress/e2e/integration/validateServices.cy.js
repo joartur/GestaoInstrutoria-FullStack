fbe4cfea514ec7ec9e967c097aa92e2eea44756f
Clark <flavioalexandre115@gmail.com>
2024-06-07 16:31:55 -0300

    feat: criando testes de validar registro

--- GestaoInstrutoriaFront/cypress/e2e/integration/validateServices.cy.js
+++ GestaoInstrutoriaFront/cypress/e2e/integration/validateServices.cy.js
@@ -2,14 +2,14 @@ describe('Testes de Coordenador validar serviços para instrutor', () => {
     it('Validar um registro', () => {
         cy.visit('http://localhost:3000/')
 
-        cy.get('td.status-sphere-td').first() 
-        .find('a')
-        .click();
+        cy.get('td.status-sphere-td').first() .find('a').click();
 
         cy.url().should('include', '/validate/');
 
-        cy.get('td')
-        .find('td')
-        .click();
+        cy.get('a[title="VALIDAR SERVIÇO EDUCACIONAL"]').click();
+
+        cy.get('button.confirm-btn').click();
+
+        cy.get('h2').should('have.text', 'confirmação');        
     })
 });
