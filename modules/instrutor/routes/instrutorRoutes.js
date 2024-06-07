ce2388ddc7d2840acdec7f83f0560097fc2c5f52
iohana <iohana.ma31@gmail.com>
2024-06-07 16:26:21 -0300

    reafctor: tentativa de descontrução.

--- modules/instrutor/routes/instrutorRoutes.js
+++ modules/instrutor/routes/instrutorRoutes.js
@@ -1,53 +1,53 @@
 const express = require('express');
 const override = require('method-override');
-const instrutorController = require('../controllers/instrutorController');
+const { InstrutorController } = require('../controllers/instrutorController');
 const router = express.Router();
 
 router.use(override('_method'));
 
 // rota get da page home do instrutor
 router.get('/:matriculaInstrutor',(req, res) => {
-    instrutorController.home(req, res);
+    InstrutorController.home(req, res);
 })
 
-// rota get da page home do instrutor
+// rota get do perfil do instrutor
 router.get('/perfil/:matriculaInstrutor',(req, res) => {
-    instrutorController.perfil(req, res);
+    InstrutorController.perfil(req, res);
 })
 
 //rota para a lista de todos os registro do instrutor
 router.get('/registros/:matriculaInstrutor',(req, res) => {
-    instrutorController.listarRegistros(req, res);
+    InstrutorController.listarRegistros(req, res);
 })
 
 //rota para a lista dos registros filtrados do instrutor
 router.post('/registros/:matriculaInstrutor',(req, res) => {
-    instrutorController.filtroRegistros(req, res);
+    InstrutorController.filtroRegistros(req, res);
 })
 
 //rota post para criar um novo registro do instrutor
 router.post('/registro/:matriculaInstrutor',(req, res) => {
-    instrutorController.cadastrarRegistro(req, res);
+    InstrutorController.cadastrarRegistro(req, res);
 })
 
 //rota para visualização de um registro
 router.get('/registro/:matriculaInstrutor/:registroId',(req, res) => {
-    instrutorController.visualizarRegistro(req, res);
+    InstrutorController.visualizarRegistro(req, res);
 })
 
 //rota para ediçao de informações
 router.put('/registro/:matriculaInstrutor/:registroId',(req, res)=>{
-    instrutorController.editarRegistro(req, res);
+    InstrutorController.editarRegistro(req, res);
 })
 
 //rota para exclusão de um registro
 router.delete('/registro/:matriculaInstrutor/:registroId',(req, res)=>{
-    instrutorController.excluirRegistro(req, res);
+    InstrutorController.excluirRegistro(req, res);
 })
 
 //rota para listar as atividade de serviço educacional
 router.get('/servicos/:matriculaInstrutor',(req, res) => {
-    instrutorController.listaAtvs(req, res);
+    InstrutorController.listaAtvs(req, res);
 })
 
 module.exports = router;
